20faf0fecd0fedb74b14a2e7bf5515042a4308c2
barkha yadav <143165059+barkhayadav12@users.noreply.github.com>
2026-01-16 15:20:05 +0530

    Update CartContext.jsx

--- frontend/src/context/CartContext.jsx
+++ frontend/src/context/CartContext.jsx
@@ -7,13 +7,13 @@ const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
 
   const fetchCart = async () => {
-    const res = await axios.get("http://localhost:5000/api/cart");
+    const res = await axios.get("https://assignment-mo91.onrender.com/api/cart");
     setCart(res.data);
   };
 
   const addToCart = async (productId) => {
     try {
-      await axios.post("http://localhost:5000/api/cart", { productId });
+      await axios.post("https://assignment-mo91.onrender.com/api/cart", { productId });
       fetchCart();
       return true;
     } catch (err) {
@@ -23,12 +23,12 @@ const CartProvider = ({ children }) => {
   };
 
   const decreaseQty = async (id) => {
-    await axios.put(`http://localhost:5000/api/cart/decrease/${id}`);
+    await axios.put(`https://assignment-mo91.onrender.com/api/cart/decrease/${id}`);
     fetchCart();
   };
 
   const removeItem = async (id) => {
-    await axios.delete(`http://localhost:5000/api/cart/${id}`);
+    await axios.delete(`https://assignment-mo91.onrender.com/api/cart/${id}`);
     fetchCart();
   };
 
