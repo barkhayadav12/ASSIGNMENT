a567ef3f8c7f782cc3f3d786b897da25d9b6eb75
barkha yadav <143165059+barkhayadav12@users.noreply.github.com>
2026-01-16 15:20:49 +0530

    Update WishlistContext.jsx

--- frontend/src/context/WishlistContext.jsx
+++ frontend/src/context/WishlistContext.jsx
@@ -7,19 +7,19 @@ const WishlistProvider = ({ children }) => {
   const [wishlist, setWishlist] = useState([]);
 
   const fetchWishlist = async () => {
-    const res = await axios.get("http://localhost:5000/api/wishlist");
+    const res = await axios.get("https://assignment-mo91.onrender.com/api/wishlist");
     setWishlist(res.data);
   };
 
   const addToWishlist = async (productId) => {
-    await axios.post("http://localhost:5000/api/wishlist", { productId });
+    await axios.post("https://assignment-mo91.onrender.com/api/wishlist", { productId });
     fetchWishlist();
   };
 
   const removeFromWishlist = async (productId) => {
   const item = wishlist.find((w) => w.product._id === productId);
   if (item) {
-    await axios.delete(`http://localhost:5000/api/wishlist/${item._id}`);
+    await axios.delete(`https://assignment-mo91.onrender.com/api/wishlist/${item._id}`);
     fetchWishlist();
   }
 };
